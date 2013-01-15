64e4fbb7b00dc21be74b409c38c67bedaf648020
Fenfei Zeng <fenfei.zengff@alibaba-inc.com>
2013-01-15 23:02:39 +0800

    generally working

---
+++ www/console/browerDetect.js
@@ -0,0 +1,9 @@
+function getDeviceName (ua) {
+	if(ua.indexOf('iPhone')!=-1){
+		return 'iPhone';
+	}else if(ua.indexOf('iPad')!=-1){
+		return 'iPad';
+	}else if(ua.indexOf('Android')!=-1){
+		return 
+	}
+}
